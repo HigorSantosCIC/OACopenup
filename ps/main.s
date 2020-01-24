1f82c95fcdaf5213ce53a3533d605c91e5ac3bc4
Higor Gabriel <38538335+HigorSantosCIC@users.noreply.github.com>
2020-01-23 16:33:53 -1000

    BORA DESGRAÇA

---
+++ ps/main.s
@@ -0,0 +1,47 @@
+.data
+
+x: .string "x: "
+y: .string "y: "
+
+
+.text
+
+
+ 
+MAIN: 
+lw a0 VETOR
+jal SORTEIO	
+		
+la a0,VETOR
+jal SORT
+	
+li a0 5	
+ebreak
+call POSICAO$SHOW
+
+li a7,10
+ecall
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+.include "coding.s"
