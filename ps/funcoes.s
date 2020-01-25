9d337f46d85f2af44151208dbc0fcf0999dd5d48
Higor Gabriel <38538335+HigorSantosCIC@users.noreply.github.com>
2020-01-24 21:05:14 -0300

    Update funcoes.s

--- ps/funcoes.s
+++ ps/funcoes.s
@@ -1,7 +1,6 @@
 .data
 pula: .string "\n"
-VETOR:  .word 10
-
+VETOR:  .word 5
 .text
 ###########################################################################################
 # ARGUMENTOS: A0 = NUMERO DE PONTOS 
@@ -9,42 +8,43 @@ VETOR:  .word 10
 # @RETURN NENHUM
 #
 ###########################################################################################
-
+	
+	## S2 = Vetor desordenado
+	## S4 = N
+	## S3 = Vetor ordenado
 	SORTEIO:
-	mv 	s0 a0
-	addi 	s1 zero 0
+	add 	s1 zero zero
 	
 	SORTEIO$FOR:
 	addi 	a7 zero 42 #ecall que chama os rand
 	li 	a1 319 #definindo o teto
 	li 	a0 1
 	ecall	
-	mv 	s2 a0 ## salvando o valor aleatorio em s2
+	mv 	t0 a0 ## salvando o valor aleatorio em s2
 			
 	addi 	a7 zero 42 #ecall que chama os rand
 	li 	a1 239 #definindo o teto
 	li 	a0 1
 	ecall	
-	mv 	s3 a0 ## salvando o valor aleatorio em s3
+	mv 	t1 a0 ## salvando o valor aleatorio em s3
 	
-	slli	s2 s2 16 #movendo os 16 bits 
-	or	s2 s2 s3 #fazendo um or pra juntar na word
+	slli	t0 t0 16 #movendo os 16 bits 
+	or	t0 t0 t1 #fazendo um or pra juntar na word
 	
-	la	t0 VETOR #carregando o endereço de V em t0
 	slli	t1 s1 2 #multiplicando por 4 s1, e salvando em t1
-	add	t0 t0 t1 #colocando o valor de t1 em t0
+	add	t2 t1 s2 #colocando o valor de t1 em t0
 	
 	 
-	bge 	s1 s0 SORTEIO$MORREU #Verificando se s0 é maior que s1, se for ele morre
-	sw	s2 0(t0) #carregando na memoria s2
+	bge 	s1 s4 SORTEIO$MORREU #Verificando se s0 é maior que s1, se for ele morre
+	sw	t0 0(t2) #carregando na memoria s2
 	
 				
-	li a7, 1
-	ecall
+#	li a7, 1
+#	ecall
 			
-	la a0, pula
-	li a7 4
-	ecall
+#	la a0, pula
+#	li a7 4
+#	ecall
 	
 	addi 	s1 s1 1 #add 1 no contador s1
 	j SORTEIO$FOR #retorna o loop
@@ -68,25 +68,15 @@ VETOR:  .word 10
 ##############################################################	
 	
 	
-#########################################################################################
-#
-# ARGUMENTO: PEGA OS PONTOS NO VETOR E ORDENA, VERIFICANDO A PRECEDENCIA DA ORDEM CRESCENTE, SALVA O VALOR DE A0 EM S2
-# E SALVA O VALOR DE A1 EM S3
-# @BRIEF:
-# @RETORNO:
-#
-#########################################################################################
-	
-	SWAP:	slli t1,a1,2
+SWAP:	slli t1,a1,2
 	add t1,a0,t1
 	lw t0,0(t1)
 	lw t2,4(t1)
 	sw t2,0(t1)
 	sw t0,4(t1)
 	ret
-	
-	SORT:	
-	addi sp,sp,-20
+
+SORT:	addi sp,sp,-20
 	sw ra,16(sp)
 	sw s3,12(sp)
 	sw s2,8(sp)
@@ -95,57 +85,27 @@ VETOR:  .word 10
 	mv s2,a0
 	mv s3,a1
 	mv s0,zero
-	
-	FOR1:	
-	bge s0,s3,EXIT1
+for1:	bge s0,s3,exit1
 	addi s1,s0,-1
-	
-	FOR2:	
-	blt s1,zero,EXIT2
+for2:	blt s1,zero,exit2
 	slli t1,s1,2
 	add t2,s2,t1
 	lw t3,0(t2)
 	lw t4,4(t2)
-	bge t4,t3,EXIT2
+	bge t4,t3,exit2
 	mv a0,s2
 	mv a1,s1
 	jal SWAP
 	addi s1,s1,-1
-	j FOR2
-	
-	EXIT2:	
-	addi s0,s0,1
-	j FOR1
-	
-	EXIT1: 	lw s0,0(sp)
+	j for2
+exit2:	addi s0,s0,1
+	j for1
+exit1: 	lw s0,0(sp)
 	lw s1,4(sp)
 	lw s2,8(sp)
 	lw s3,12(sp)
 	lw ra,16(sp)
 	addi sp,sp,20
-	ret
-
-	SHOW:	
-	mv t0,a0
-	mv t1,a1
-	mv t2,zero
-
-	LOOP1: 	
-	beq t2,t1,FIM1
-	li a7,1
-	lw a0,0(t0)
-	ecall
-	li a7,11
-	li a0,9
-	ecall
-	addi t0,t0,4
-	addi t2,t2,1
-	j LOOP1
-
-	FIM1:	
-	li a7,11
-	li a0,10
-	ecall
 	ret
 	
 #################################################################################################################
@@ -157,7 +117,7 @@ VETOR:  .word 10
 	
 	POSICAO$SHOW:
 	mv s2 a0   # n em s2
-	la s4 VETOR
+	mv s4 a1   # Vetor em s4
 	add s3 zero zero
 	POSICAO$LOOP:
 	bge s3 s2 POSICAO$ENDLOOP
@@ -170,7 +130,7 @@ VETOR:  .word 10
 	ecall
 	
 	li a7 1
-	mv a0 s0
+	mv a0 s1
 	ecall
 	
 #	li a7 4
@@ -182,7 +142,7 @@ VETOR:  .word 10
 	ecall
 	
 	li a7 1
-	mv a0 s1
+	mv a0 s0
 	ecall
 	
 	li a7 4
