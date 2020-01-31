d77d9fc55e73c7e0140e5e41b49f72405f390147
Higor Gabriel <38538335+HigorSantosCIC@users.noreply.github.com>
2020-02-01 10:01:44 +1100

    Add files via upload
    RODA NA PLACA ESSE DEMONIO

---
+++ Versao que roda na placa/funcoes.s
@@ -0,0 +1,318 @@
+.data
+pula: .string "\n"
+VETOR:  .word 5
+ordem_pontos: .word 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+.text
+###########################################################################################
+# ARGUMENTOS: A0 = a6UMERO DE PONTOS 
+# @BRIEF ELE SELECIONA O "n" a6UMERO DE PONTOS ALEATORIOS QUE SERAO GERADOS PARA AS COORDENADAS X E Y SALVOS a6O VETOR
+# @RETURN a6ENHUM
+#
+###########################################################################################
+	
+	## S2 = Vetor desordenado
+	## S4 = a6
+	## S3 = Vetor ordenado
+SORTEIO:
+add 	s1 zero zero
+	
+SORTEIO_FOR:
+addi 	a7 zero 41 #ecall que chama os rand
+ecall
+mv 	t0, a0
+li 	t3 319
+remu 	t0, a0, t3  	
+			
+addi 	a7 zero 41 #ecall que chama os rand
+ecall
+mv 	t1 a0
+li 	t4 239
+remu 	t1, a0, t4	
+
+slli	t0 t0 16 #movendo os 16 bits 
+or	t0 t0 t1 #fazendo um or pra juntar na word
+
+slli	t1 s1 2 #multiplicando por 4 s1, e salvando em t1
+add	t2 t1 s2 #colocando o valor de t1 em t0
+
+ 
+bge 	s1 s4 SORTEIO_MORREU #Verificando se s0 é maior que s1, se for ele morre
+sw	t0 0(t2) #carregando na memoria s2
+	addi 	s1 s1 1 #add 1 no contador s1
+j SORTEIO_FOR #retorna o loop
+	
+SORTEIO_MORREU:
+ret
+##############################################################
+#
+# ::::::::::::::::::FUNÇÃO DE SORT::::::::::::::::::
+#
+#
+##############################################################	
+	
+	
+SWAP:	
+slli 	t1 a1 2
+add 	t1 a0 t1
+lw 	t0 0(t1)
+lw 	t2 4(t1)
+sw 	t2 0(t1)
+sw 	t0 4(t1)
+ret
+
+SORT:	
+addi 	sp sp -20
+sw 	ra 16(sp)
+sw 	s3 12(sp)
+sw 	s2 8(sp)
+sw 	s1 4(sp)
+sw 	s0 0(sp)
+mv 	s2 a0
+mv 	s3 a1
+mv 	s0 zero
+for1:
+bge 	s0 s3 exit1
+addi 	s1 s0 -1
+for2:	
+blt 	s1 zero exit2
+slli 	t1 s1 2
+add 	t2 s2 t1
+lw 	t3 0(t2)
+lw 	t4 4(t2)
+bge 	t4 t3 exit2
+mv 	a0 s2
+mv 	a1 s1
+jal SWAP
+addi	s1 s1 -1
+j for2
+exit2:	
+addi s0 s0 1
+j for1
+exit1: 	
+lw 	s0 0(sp)
+lw 	s1 4(sp)
+lw 	s2 8(sp)
+lw 	s3 12(sp)
+lw 	ra 16(sp)
+addi 	sp sp 20
+ret
+	
+#################################################################################################################
+# ACESSA A MEMORIA ONDE ESTÃO ARMAZENADOS OS VALORES DO VETOR
+# CARREGA 16 BITS EM S0 DO ENDEREÇO EM T0
+# CARREGA OS OUTROS 16 BITS EM S1
+#
+#################################################################################################################	
+	
+POSICAO_SHOW:
+mv 	s2 a0   # n em s2
+mv 	s4 a1   # Vetor em s4
+add 	s3 zero zero
+POSICAO_LOOP:
+bge 	s3 s2 POSICAO_ENDLOOP
+lh 	s0 0(s4)
+lh 	s1 2(s4)
+	
+#li 	a7 4
+#la 	a0 x
+#ecall
+	
+#li 	a7 1
+mv 	a0 s1
+#ecall
+
+#li 	a7 4
+#la 	a0 y
+#ecall
+	
+#li 	a7 1
+mv 	a0 s0
+#ecall
+	
+#li 	a7 4
+#la 	a0 pula
+#ecall
+		
+addi 	s4 s4 4
+addi 	s3 s3 1
+	
+j POSICAO_LOOP
+	
+POSICAO_ENDLOOP:	
+ret
+	
+
+###########################################################################################
+# ACESSAR_VETOR
+# ARGUMENTOS: a0 = ponteiro para o vetor, a1 = a6UMERO DA POSICAO DO VETOR A SER ACESSADO
+# @RETURN A0 VALOR DE X, A1 VALOR DE Y
+###########################################################################################
+ACESSAR_VETOR:
+addi 	sp sp -4
+sw 	ra 0(sp)
+	
+slli 	a1 a1 2
+add 	t0 a0 a1
+lh   	a1 0(t0)
+lh	a0 2(t0)
+
+ACESSAR_VETOR_END:
+lw 	ra 0(sp)
+addi 	sp sp 4
+ret
+	
+
+###########################################################################################
+# LIGA_PONTOS
+# ARGUMENTOS: a0 = ponteiro para o vetor, a1 = NUUMERO DE PONTOS
+# @RETURN Retorna a0 ponteiro para a pilha com o inicio do vetor 
+###########################################################################################
+LIGA_PONTOS:
+addi 	sp sp -32
+fsw  	fs0 28(sp)
+sw 	s11 24(sp)
+sw 	s10 20(sp)
+sw 	s3 16(sp)
+sw 	s2 12(sp)
+sw 	s1 8(sp)
+sw 	s0 4(sp)
+sw 	ra 0(sp)
+
+addi 	s0 zero 1 
+add 	s1 zero s6
+
+
+# Criar vetor com a sequencia de pontos a serem ligados ########
+la 	s2 ordem_pontos
+slli 	t0 s1 2
+add 	s2 s2 t0
+sw	zero 0(s2) 	# Salva o primeiro ponto que � o valor 0
+addi	s2 s2 -4	# Salva o ponteiro para os numeros abaixo
+addi	s3 s2 8 	# Salva o ponteiro para os numeros acima
+# Criar vetor com a sequencia de pontos a serem ligados ########
+
+mv 	s10 a0
+mv 	s11 a1
+
+mv 	a0 s10
+addi	a1 zero 0
+add 	a2 zero s6
+addi 	a2 a2 -1
+call coeficiente_angular
+
+fmv.s	fs0 fa0		# coeficiente angular 
+
+LIGA_PONTOS_for: 
+bge 	s0 s1 LIGA_PONTOS_end_for
+mv 	a0 s10	 # ponteiro para as coordenadas ordenadas
+add 	a1 zero s0
+addi	a2 zero 0
+call coeficiente_angular
+
+fle.s 	t0 fa0 fs0
+
+beq 	t0 zero LIGA_PONTOS_acima
+
+LIGA_PONTOS_abaixo:
+sw	s0 0(s2)	# Salva o numero do ponto
+addi	s2 s2 -4	# desloca o ponteiro para a posicao anterior
+j	LIGA_PONTOS_endif
+
+LIGA_PONTOS_acima:
+sw	s0 0(s3)	# salva o numero do ponto
+addi	s3 s3 4		# desloca o ponteiro para a proxima posicao 
+
+LIGA_PONTOS_endif:	
+addi 	s0 s0 1
+j	LIGA_PONTOS_for
+
+LIGA_PONTOS_end_for:
+
+# Deve adicionar e subtrair pois os ponteiros são adicionados a mais no loop
+addi	a0 s2 4
+
+LIGA_PONTOS_end:
+flw 	fs0 28(sp)
+lw 	s11 24(sp)
+lw 	s10 20(sp)
+lw 	s3 16(sp)
+lw 	s2 12(sp)
+lw 	s1 8(sp)
+lw 	s0 4(sp)
+lw 	ra 0(sp)
+addi 	sp sp 32
+ret
+
+###########################################################################################
+# print_LIGA_PONTOS
+# ARGUMENTOS: a0 = ponteiro para inicio do vetor, a1 = frame a ser printada, a2 = ponteiro para as coordenadas
+# @RETURN nada
+###########################################################################################
+
+print_LIGA_PONTOS:
+addi 	sp sp -20
+lw 	s3 16(sp)
+lw 	s2 12(sp)
+lw 	s1 8(sp)
+lw 	s0 4(sp)
+sw 	ra 0(sp)
+
+mv 	s0 a0
+addi 	s1 zero 0
+add 	s2 zero s6
+mv 	s3 a2
+#mv 	a4 a1
+
+mv 	t2 s0
+lw 	t1 0(t2)
+slli 	t3 s2 2
+add	t2 t2 t3
+sw 	t1 0(t2) 	# Armazena o primeiro ponto no fim do vetor
+
+print_LIGA_PONTOS_for:
+beq 	s1 s2 print_LIGA_PONTOS_for_end 	
+lw 	t0 0(s0)  	# pega o valor da memoria
+mv 	a0 s3		# Ponteiro para o vetor
+mv 	a1 t0		# Posicao do vetor
+call ACESSAR_VETOR
+
+mv 	a2 a0		# a2 = x0
+mv 	a3 a1		# a3 = y0
+
+lw 	t0 4(s0)  	# pega o valor da memoria
+mv 	a0 s3		# Ponteiro para o vetor
+mv 	a1 t0		# Posicao do vetor
+call ACESSAR_VETOR
+
+# a0 = x1
+# a1 = y1
+# a2 = x0
+# a3 = y0
+
+#li 	a4 254 #cor do print, como aleatorizar?
+addi a4 zero 0X010
+addi a4 zero 0XB
+#jal	cor_aleatoria
+li	a5 0
+li 	a7 147
+ecall
+
+addi 	s0 s0 4 # proxima posicao do vetor
+addi 	s1 s1 1
+j	print_LIGA_PONTOS_for
+
+print_LIGA_PONTOS_for_end:
+sw 	s3 16(sp)
+sw 	s2 12(sp)
+sw 	s1 8(sp)
+sw 	s0 4(sp)
+lw 	ra 0(sp)
+addi 	sp sp 20
+ret
+
+clear_screen:
+addi 	a0 zero 0xFF 		## printa tela de branco
+addi 	a1 zero 0
+addi 	a7 zero 148
+ecall
+
---
+++ Versao que roda na placa/main.s
@@ -0,0 +1,80 @@
+.eqv N 3
+
+.data
+
+#x: .string " x: "
+#y: .string " y: "
+
+.text
+
+## S11 = N
+## S10 = Vetor
+##MEU A6 É O N DEUS
+
+
+MAIN: 
+la 	t0 exceptionHandling
+csrw 	t0 utvec
+csrwi	ustatus 1
+#li 	s6 N
+addi s6 s6 3
+addi a6 zero 20
+
+
+loop_main:
+
+beq s6 a6 end_int
+jal clear_screen
+
+add 	s11 s11 s6
+slli 	t0 s11 2
+sub 	sp sp t0  	#Alocando espaço na pilha para o vetor
+add 	s10 zero sp 	# Ponteiro para o vetor
+
+add 	s2 s10 zero
+add 	s4 s11 zero
+jal SORTEIO	
+
+add	a0 zero s10
+add 	a1 zero s11
+jal SORT
+
+add 	a1 zero s10
+add 	a0 zero s11
+call POSICAO_SHOW
+
+mv 	a0 s10
+add a1 zero s6
+call LIGA_PONTOS
+
+# a0 ponteiro do inicio do vetor
+addi 	a1 zero 0
+mv	a2 s10
+call print_LIGA_PONTOS
+addi	s6 s6 1
+
+li a0 1000
+li a7 32
+ecall
+
+call loop_main
+
+end_int:
+jal end_int
+#li a7 10
+#ecall
+
+
+
+
+
+
+
+
+
+
+
+
+.include "funcoes.s"
+.include "a_da_reta.s"
+.include "SYSTEMv17b.s"
