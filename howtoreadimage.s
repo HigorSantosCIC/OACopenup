b56e4ab7b3e04bd56265300417d37e19bb25480f
Higor Gabriel <38538335+HigorSantosCIC@users.noreply.github.com>
2020-02-08 14:49:30 -0300

    Update howtoreadimage.s

--- howtoreadimage.s
+++ howtoreadimage.s
@@ -16,3 +16,76 @@ LOOP1: 	beq t1,t2,FIM	# Se for o último endereço então sai do loop
 
 FIM:	li a7,10		# syscall de exit
 	ecall
+.data
+FLOAT: 	.float 	3.14159265659
+msg1: 	.string "Snake OAC/VERAO 2020"
+msg_start: .string "Inicializando o jogo: ..."
+msg2: .string "HIGHSCORE:"
+msg3: .string "Level Speed:"
+buffer: .string "                                "
+
+.text
+ 	la tp,exceptionHandling	# carrega em tp o endere�o base das rotinas do sistema ECALL
+ 	csrw tp,utvec 		# seta utvec para o endere�o tp
+ 	csrsi ustatus,1 	# seta o bit de habilita��o de interrup��o em ustatus (reg 0)																																																				
+	
+	
+	jal CLSV
+	jal CLS
+	ebreak
+	jal PRINTSTR1
+	jal SLEEP
+	li a7,10
+	ecall
+				
+# CLS Clear Screen
+CLS:	li a0,0x00
+	li a7,148
+	li a1,0
+	ecall
+	ret
+	
+CLSV:	li a7,141
+	ecall
+	li a7,148
+	li a1,0
+	ecall
+	ret
+
+				
+# syscall print string
+PRINTSTR1: li a7,104
+	la a0,msg1
+	li a1,0
+	li a2,0
+	li a3,0x0038
+	li a4,0
+	ecall
+	ret		
+# syscall sleep
+SLEEP:	li t0,5
+LOOPHMS:li a0,1000   # 1 segundo
+	li a7,132
+	ecall
+	addi t0,t0,-1
+	#print seg
+	mv a0,t0
+	li a7,101
+	li a1,120
+	li a2,120
+	li a3,0x0038
+	li a4,0
+	ecall
+	bne t0,zero,LOOPHMS	
+	ret
+
+
+
+
+.include "SYSTEMv17b.s"
+
+
+
+
+
+
