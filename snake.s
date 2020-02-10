a01eb6841779faed7fac36b6aa757fabf89102ea
Higor Gabriel <38538335+HigorSantosCIC@users.noreply.github.com>
2020-02-10 17:30:22 -0300

    Update snake.s

--- snake.s
+++ snake.s
@@ -1,16 +1,75 @@
 .data
-msg1: 	.string "SNAKE "
+.include "snakegame2a.s"
+.include "snakegame2b.s"
+msg1: .string "SNAKE "
 roda_pe: .string "OAC VERAO"
 msg_start: .string "Inicializando o jogo: "
 msg_high: .string "HIGHSCORE:"
 msg_speed: .string "Level Speed:"
 buffer: .string "                                "
 
-.text
-la t0,exceptionHandling	# carrega em tp o endere?o base das rotinas do sistema ECALL
-csrw t0,utvec 		# seta utvec para o endere?o tp
-csrsi ustatus,1 	# seta o bit de habilita??o de interrup??o em ustatus (reg 0)																																																				
 
+.text
+la tp exceptionHandling# carrega em tp o endere�o base das rotinas do sistema ECALL
+csrw tp utvec # seta utvec para o endere�o tp
+csrsi ustatus 1 # seta o bit de habilita��o de interrup��o em ustatus (reg 0)
+# Preenche a tela de vermelho
+li t0 2
+j TelaInicial
+
+
+# Carrega a imagem1
+TelaInicial:
+li t1 0xFF000000# endereco inicial da Memoria VGA - Frame 0
+li t2 0xFF012C00# endereco final 
+la s1 snakegame2a# endere�o dos dados da tela na memoria
+addi s1 s1 8# primeiro pixels depois das informa��es de nlin ncol
+LOOP1: beq t1 t2 Entrada# Se for o �ltimo endere�o ent�o sai do loop
+lw t3 0(s1)# le um conjunto de 4 pixels : word
+sw t3 0(t1)# escreve a word na mem�ria VGA
+addi t1 t1 4# soma 4 ao endere�o
+addi s1 s1 4
+j LOOP1# volta a verificar
+
+#enter para iniciar o menu
+Entrada: 
+li a7 105
+ecall
+
+Menu:
+li t0 2
+li t1 0xFF000000# endereco inicial da Memoria VGA - Frame 0
+li t2 0xFF012C00# endereco final 
+la s1 snakegame2b
+addi s1 s1 8# cor vermelho|vermelho|vermelhor|vermelho
+
+LOOP3: 
+beq t1 t2 FIM
+lw t3 0(s1)# Se for o �ltimo endere�o ent�o sai do loop
+sw t3 0(t1)# escreve a word na mem�ria VGA
+addi t1 t1 4
+addi s1 s1 4# soma 4 ao endere�o
+j LOOP3# volta a verificar
+
+FIM:
+li a7 105
+ecall
+bge a0 t0 sai
+j inicio
+sai:
+li t1 0xFF000000# endereco inicial da Memoria VGA - Frame 0
+li t2 0xFF012C00# endereco final 
+li t3 0x00000000# cor vermelho|vermelho|vermelhor|vermelho
+LOOP: 
+beq t1 t2 sai2# Se for o �ltimo endere�o ent�o sai do loop
+sw t3 0(t1)# escreve a word na mem�ria VGA
+addi t1 t1 4# soma 4 ao endere�o
+j LOOP# volta a verificar
+sai2:   
+li a7 10
+ecall
+
+inicio:
 jal CLS
 jal str_snake
 jal str_start
@@ -31,105 +90,105 @@ jal top_bord
 
 ##### na funcao str score int t0 = pontuacao
 # na funcao str speed t1 = pontuacao
+j comecacobra
+call sorteia_comida
 
-li a7 10
-ecall
-																																		
 # CLS Clear Screen
 CLS:
-li a0,0x00
-li a7,148
-li a1,0
+li a0 0x00
+li a7 148
+li a1 0
 ecall
 ret
-	
-CLSV:	
-li a7,141
+
+CLSV:
+li a7 141
 ecall
-li a7,148
-li a1,0
+
+li a7 148
+li a1 0
 ecall
 ret
 
 #########################
 # a1 =  X (0 ate 320)   #
 # a2 =  Y (0 ate 240)   #
-######################### 		
+######################### 
 
 # syscall "SNAKE"
 str_snake: 
-li a7,104
-la a0,msg1
-li a1,2
-li a2,0
-li a3,0x0038
-li a4,0
+li a7 104
+la a0 msg1
+li a1 2
+li a2 0
+li a3 0x0038
+li a4 0
 ecall
-ret		
+ret
 
 # sycall "msg_start"
 str_start: 
-li a7,104
-la a0, msg_start
+li a7 104
+la a0  msg_start
 li a1 80
 li a2 102
-li a3,0x0038
-li a4,0
+li a3 0x0038
+li a4 0
 ecall
-ret		
+ret
 
 #sycall "HIGHSCORE"
 str_high: 
-li a7,104
-la a0, msg_high
+li a7 104
+la a0  msg_high
 li a1 2
 li a2 70
-li a3,0x0038
-li a4,0
+li a3 0x0038
+li a4 0
 ecall
-ret		
+ret
 
 #sycall "LEVEL SPEED"
 str_speed: 
-li a7,104
-la a0, msg_speed
+li a7 104
+la a0  msg_speed
 li a1 2
 li a2 106
-li a3,0x0038
-li a4,0
+li a3 0x0038
+li a4 0
 ecall
-ret		
+ret
 
 #sycall "RODA PE"
 
 str_roda:
-li a7,104
-la a0, roda_pe
+li a7 104
+la a0 roda_pe
 li a1 2
 li a2 230
-li a3,0x0038
-li a4,0
+li a3 0x0038
+li a4 0
 ecall
-ret		
+ret
 
 # syscall sleep
-SLEEP:	
-li t0, 2
+SLEEP:
+li t0 2
 LOOPHMS:
-li a0,1000   # 1 segundo
-li a7,132
+li a0 1000   # 1 segundo
+li a7 132
 ecall
-addi t0,t0,-1
+addi t0 t0 -1
 
 #print seg
-mv a0,t0
-li a7,101
-li a1,162
-li a2,112
-li a3,0x0038
-li a4,0
+mv a0 t0
+li a7 101
+li a1 162
+li a2 112
+li a3 0x0038
+li a4 0
 ecall
-bne t0,zero,LOOPHMS	
+bne t0 zero LOOPHMS
 ret
 
 #syscall pontuacao
@@ -144,6 +203,7 @@ li a4 0
 ecall 
 ret
 
+#sycall contador velocidade nivel
 str_speed_int:
 li t1 0
 li a7 101
@@ -156,12 +216,13 @@ ecall
 ret
 
 #Draw map
+
 left_bord:
 li a7 147
-li a0 105
-li a1 5
-li a2 105
-li a3 235
+li a0 100
+li a1 10
+li a2 100
+li a3 230
 li a4 0x0038
 li a5 0
 ecall
@@ -169,10 +230,10 @@ ret
 
 top_bord:
 li a7 147
-li a0 105
-li a1 5
-li a2 314
-li a3 5
+li a0 100
+li a1 10
+li a2 310
+li a3 10
 li a4 0x0038
 li a5 0
 ecall
@@ -180,10 +241,10 @@ ret
 
 right_bord:
 li a7 147
-li a0 314
-li a1 5
-li a2 314
-li a3 234
+li a0 310
+li a1 10
+li a2 310
+li a3 230
 li a4 0x0038
 li a5 0
 ecall
@@ -191,23 +252,493 @@ ret
 
 botton_bord:
 li a7 147
-li a0 105
-li a1 234
-li a2 314
-li a3 234
+li a0 100
+li a1 230
+li a2 310
+li a3 230
 li a4 0x0038
 li a5 0
+ecall
+ret
+
+comecacobra:
+ 
+li t3 100
+li t4 310
+li t5 10
+li t6 230
+jal sorteia_comida
+li a7 147
+li a0 160
+li a1 120
+li a2 170
+li a3 120
+li a4 0x0038
+li a5 0
+addi sp sp -20 #pilha posiacao inicial da cobra
+sw a0 16(sp)
+sw a1 12(sp)
+sw a2 8(sp)
+sw a3 4(sp)
+sw a5 0(sp)
+ecall
+
+li s3 0 #primeira vez que vira pra baixo
+li s4 1 #primeira vez que vira pra direita
+li s5 0 #primeira vez que vira pra esquerda
+li s6 0 #primeira vez que vira pra cima
+
+li s1 1 #se ta pra cima
+li s0 1 #se ta virado pra esquerdA
+
+#input flag
+
+input:
+
+li t1 0xff200000 #Endere�o do MMIO
+lw t0 0(t1)  #Le o bit de controle do teclado
+andi t0 t0 0x0001 #Mascara o bit menos significativo
+
+beq t0 zero loop_anterior # = estado anterior #nao tem tecla pressionada entao volta ao loop  ARMAZENAR O BAGUI
+lw t2 4(t1)#Le o valor da tecla
+
+
+#PRA EFETIVAMENTE COLOCAR EFEITO CRESCENTE BASTA POR UM DECREMENTADOR NO SLEEP
+loop_anterior:
+mv s2 t2
+li t3 1000
+add a0 t3 t3   # 1 segundo
+li a7 132
+ecall
+
+li t1 0x61 # a
+beq s2 t1 esquerda
+
+li t1 0x64 # d
+beq s2 t1 direita
+
+li t1 0x73 # s
+beq s2 t1 desce
+
+li t1 0x77 # w
+beq s2 t1 cima
+ret
+
+li a7 10
+ecall
+
+limpa:
+lw a5 0(sp)
+lw a3 4(sp)
+lw a2 8(sp)
+lw a1 12(sp)
+lw a0 16(sp)
+li a4 0x0000
+li a7 147
+ecall
+ret
+
+cima:
+beq s6 zero viraCima
+jal limpa
+lw a5 0(sp)
+lw a3 4(sp)
+lw a2 8(sp)
+lw a1 12(sp)
+lw a0 16(sp)
+li a4 0x0038
+add a1 zero a3 
+addi a3 a3 -10
+sw a0 16(sp)
+sw a1 12(sp)
+sw a2 8(sp)
+sw a3 4(sp)
+sw a5 0(sp)
+li a7 147 
+ecall
+jal COLISAO
+li s3 0 
+li s4 0
+li s5 0
+li s1 1
+
+jal Colisao_comida
+j input
+
+viraCimaEsq:
+jal limpa
+
+lw a5 0(sp)
+lw a3 4(sp)
+lw a2 8(sp)
+lw a1 12(sp)
+lw a0 16(sp)
+add a0 zero a2 #aqui
+li a2 0
+add a2 a0 zero
+addi a3 a3 -10
+li a4 0x0038
+sw a0 16(sp)
+sw a1 12(sp)
+sw a2 8(sp)
+sw a3 4(sp)
+sw a5 0(sp)
+
+li a7 147 
+ecall
+jal COLISAO
+addi s6 s6 1
+li s3 0 
+li s4 0
+li s5 0
+li s1 1
+j input
+
+viraCima:
+beq s6 zero viraCimaEsq
+
+jal limpa
+
+lw a5 0(sp)
+lw a3 4(sp)
+lw a2 8(sp)
+lw a1 12(sp)
+lw a0 16(sp)
+add a0 zero a2 #aqui
+li a2 0
+add a2 a0 zero
+addi a3 a3 -10
+li a4 0x0038
+sw a0 16(sp)
+sw a1 12(sp)
+sw a2 8(sp)
+sw a3 4(sp)
+sw a5 0(sp)
+li a7 147 
+ecall
+jal COLISAO
+addi s6 s6 1
+li s3 0 
+li s4 0
+li s5 0
+li s1 1
+jal Colisao_comida
+j input
+
+direita:
+
+beq s4 zero viraDireitaCima
+jal limpa
+
+li a7 147
+lw a5 0(sp)
+lw a3 4(sp)
+lw a2 8(sp)
+lw a1 12(sp)
+lw a0 16(sp)
+li a4 0x0038
+add a0 zero a2
+addi a2 a2 10
+sw a0 16(sp)
+sw a1 12(sp)
+sw a2 8(sp)
+sw a3 4(sp)
+sw a5 0(sp) 
+ecall
+jal COLISAO
+li s3 0
+li s5 0
+li s0 1
+li s6 0
+jal Colisao_comida
+j input
+
+viraDireitaCima:
+beq s1 zero viraDireita
+jal limpa
+
+lw a5 0(sp)
+lw a3 4(sp)
+lw a2 8(sp)
+lw a1 12(sp)
+lw a0 16(sp)
+li a4 0x0038
+add a1 zero a3 #aqui
+li a3 0
+addi a1 a1 -10
+add a3 a1 zero
+addi a2 a2 10
+sw a0 16(sp)
+sw a1 12(sp)
+sw a2 8(sp)
+sw a3 4(sp)
+sw a5 0(sp)
+li a7 147 
+ecall
+jal COLISAO
+addi s4 s4 1
+li s5 0
+li s3 0
+li s0 1
+li s6 0
+jal Colisao_comida
+j input
+
+viraDireita:
+jal limpa
+
+lw a5 0(sp)
+lw a3 4(sp)
+lw a2 8(sp)
+lw a1 12(sp)
+lw a0 16(sp)
+li a4 0x0038
+add a1 zero a3 #aqui
+li a3 0
+add a3 a1 zero
+addi a2 a2 10
+sw a0 16(sp)
+sw a1 12(sp)
+sw a2 8(sp)
+sw a3 4(sp)
+sw a5 0(sp)
+li a7 147 
+ecall
+jal COLISAO
+addi s4 s4 1
+li s5 0
+li s3 0
+li s0 1
+li s6 0
+j input
+
+esquerda:
+beq s5 zero viraEsquerdaCima
+jal limpa
+
+lw a5 0(sp)
+lw a3 4(sp)
+lw a2 8(sp)
+lw a1 12(sp)
+lw a0 16(sp)
+li a4 0x0038
+add a0 zero a2 #aqui
+
+addi a2 a2 -10
+sw a0 16(sp)
+sw a1 12(sp)
+sw a2 8(sp)
+sw a3 4(sp)
+sw a5 0(sp)
+li a7 147 
+ecall
+jal COLISAO
+li s3 0
+li s4 0
+li s0 0
+li s6 0
+jal Colisao_comida
+j input
+
+viraEsquerdaCima:
+beq s1 zero viraEsquerda
+jal limpa
+
+lw a5 0(sp)
+lw a3 4(sp)
+lw a2 8(sp)
+lw a1 12(sp)
+lw a0 16(sp)
+li a4 0x0038
+add a1 zero a3 #aqui
+li a3 0
+add a3 a1 zero
+addi a2 a2 -10
+sw a0 16(sp)
+sw a1 12(sp)
+sw a2 8(sp)
+sw a3 4(sp)
+sw a5 0(sp)
+li a7 147 
+ecall
+jal COLISAO
+addi s5 s5 1
+li s3 0
+li s4 0
+li s0 0
+li s6 0
+jal Colisao_comida
+j input
+
+viraEsquerda:
+
+jal limpa
+
+lw a5 0(sp)
+lw a3 4(sp)
+lw a2 8(sp)
+lw a1 12(sp)
+lw a0 16(sp)
+li a4 0x0038
+add a1 zero a3 #aqui
+li a3 0
+add a3 a1 zero
+addi a2 a2 -10
+sw a0 16(sp)
+sw a1 12(sp)
+sw a2 8(sp)
+sw a3 4(sp)
+sw a5 0(sp)
+li a7 147 
+ecall
+jal COLISAO
+addi s5 s5 1
+li s3 0
+li s4 0
+li s0 0
+li s6 0
+jal Colisao_comida
+j input
+
+desce:
+beq s3 zero viraBaixo
+jal limpa
+
+lw a5 0(sp)
+lw a3 4(sp)
+lw a2 8(sp)
+lw a1 12(sp)
+lw a0 16(sp)
+li a4 0x0038
+add a1 zero a3 #aqui
+#addi a1 a1 -10 #quando come
+addi a3 a3 10
+sw a0 16(sp)
+sw a1 12(sp)
+sw a2 8(sp)
+sw a3 4(sp)
+sw a5 0(sp)
+li a7 147 
+ecall
+jal COLISAO
+li s4 0
+li s5 0
+li s6 0
+li s1 0
+jal Colisao_comida
+j input
+
+viraBaixoEsq:
+
+jal limpa
+
+lw a5 0(sp)
+lw a3 4(sp)
+lw a2 8(sp)
+lw a1 12(sp)
+lw a0 16(sp)
+add a0 zero a2 #aqui
+li a2 0
+add a2 a0 zero 
+addi a3 a3 10
+li a4 0x0038
+sw a0 16(sp)
+sw a1 12(sp)
+sw a2 8(sp)
+sw a3 4(sp)
+sw a5 0(sp)
+li a7 147 
+ecall
+jal COLISAO
+addi s3 s3 1
+li s4 0
+li s5 0
+li s6 0
+li s1 0
+jal Colisao_comida
+j input
+
+viraBaixo:
+jal limpa
+beq a6 zero viraBaixoEsq
+
+lw a5 0(sp)
+lw a3 4(sp)
+lw a2 8(sp)
+lw a1 12(sp)
+lw a0 16(sp)
+add a0 zero a2 #aqui
+li a2 0
+add a2 a0 zero
+addi a3 a3 10
+li a4 0x0038
+sw a0 16(sp)
+sw a1 12(sp)
+sw a2 8(sp)
+sw a3 4(sp)
+sw a5 0(sp)
+li a7 147 
+ecall
+jal COLISAO
+addi s3 s3 1
+li s4 0
+li s5 0
+li s6 0
+li s1 0
+jal Colisao_comida
+j input
+
+COLISAO:
+lw a2 8(sp) #x cabe�a
+lw a3 4(sp) #y cabe�a
+beq a2 t3 Menu
+beq a2 t4 Menu
+beq a3 t5 Menu
+beq a3 t6 Menu
+ret
+
+Colisao_comida:
+lw a2 8(sp)
+lw a3 4(sp)
+beq a2 s7 testay
+ret
+
+testay:
+beq a3 s8 come
+j input
+
+come: 
+j input
+
+sorteia_comida:
+#rand para x
+addi 	a7 zero 41 #ecall que chama os rand
+ecall
+mv 	t0 a0
+li 	t3 201
+remu 	t0 a0 t3  	
+addi 	t0 t0 102	
+mv s8 t1
+#rand para Y
+addi 	a7 zero 41 #ecall que chama os rand
+ecall
+mv 	t1 a0
+li 	t4 218
+remu 	t1 a0 t4	
+addi 	t1 t1 11
+mv s9 t1
+#gerado as coordenadas, hora de printar
+mv a0 s8
+mv a1 s9
+mv a2 x0
+mv a3 x0
+li a4 0x0038
+li a5 0
+li a7 147
 ecall
 ret
 
 
 
 
-
-
-
-
-
-
 .include "SYSTEMv17b.s"
-
